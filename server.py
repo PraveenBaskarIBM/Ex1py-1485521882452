e1b448086c69634765c235d5592dfcb62f03d4fa
Praveen Kumar B <pbkumar2@in.ibm.com>
2017-01-27 08:16:23 -0600

    run-6

--- server.py
+++ server.py
@@ -31,45 +31,7 @@ print('connected to mongodb!, welcome to mongodb connection, have a fun')
 import numpy as np
 import pandas as pd
 import sklearn
-#from pandas import read_csv
-#from sklearn.feature_selection import RFE
-#from sklearn.linear_model import LogisticRegression
-import cPickle as pickle
 
-# load data
-data = pd.read_csv('p_train1.csv', header=None, names=['INSTRUCTION_OBTAINED_SCORE.STAAR_v2014_Cat_4_(A.7)(A)[R]',
-                                                   'INSTRUCTION_OBTAINED_SCORE.STAAR_v2014_Cat_1_(A.11)(B)[R]','INSTRUCTION_OBTAINED_SCORE.STAAR_v2014_Cat_2_(A.3)(D)[R]',
-                                                                                                                         'INSTRUCTION_OBTAINED_SCORE.STAAR_v2014_Cat_1_(A.10)(E)[R]','INSTRUCTION_OBTAINED_SCORE.STAAR_v2014_Cat_4_(A.6)(A)[R]',
-                                                   'INSTRUCTION_OBTAINED_SCORE.STAAR_v2014_Cat_3_(A.2)(C)[R]','INSTRUCTION_OBTAINED_SCORE.STAAR_v2014_Cat_4_(A.7)(C)[R]',
-                                                   'INSTRUCTION_OBTAINED_SCORE.STAAR_v2014_Cat_2_(A.3)(C)[R]','INSTRUCTION_OBTAINED_SCORE.STAAR_v2014_Cat_3_(A.5)(A)[R]',
-                                                   'INSTRUCTION_OBTAINED_SCORE.STAAR_v2014_Cat_3_(A.2)(I)[R]','INSTRUCTION_OBTAINED_SCORE.STAAR_v2014_Cat_3_(A.5)(C)[R]',
-                                                   'INSTRUCTION_OBTAINED_SCORE.STAAR_v2014_Cat_2_(A.3)(B)[R]','INSTRUCTION_OBTAINED_SCORE.STAAR_v2014_Cat_4_(A.8)(A)[R]',
-                                                   'INSTRUCTION_OBTAINED_SCORE.STAAR_v2014_Cat_5_(A.9)(C)[R]','INSTRUCTION_OBTAINED_SCORE.STAAR_v2014_Cat_3_(A.2)(A)[R]',
-                                                   'INSTRUCTION_OBTAINED_SCORE.STAAR_v2014_Cat_5_(A.9)(D)[R]','Risk'])
-
-#print data
-data[np.isnan(data)] = 0
-
-array = data.values
-#print array
-X = array[:,0:16]
-Y = array[:,16]
-print X
-print Y
-
-# feature extraction
-model = LogisticRegression()
-rfe = RFE(model, 16)
-fit = rfe.fit(X, Y)
-
-print("Num Features: %d") % fit.n_features_
-print("Selected Features: %s") % fit.support_
-print("Feature Ranking: %s") % fit.ranking_
-print("Feature Estimator: %s") % fit.estimator
-
-with open('test3_model.pkl', 'wb') as f:
-    pickle.dump(fit, f)
-    
 ## code ends
 
 httpd = Server(("", PORT), Handler)
