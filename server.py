c1d0708b0934d3bffe9626a918dd1bb0dfae5110
Praveen Kumar B <pbkumar2@in.ibm.com>
2017-01-27 07:20:05 -0600

    run-2

--- server.py
+++ server.py
@@ -24,7 +24,7 @@ from sklearn.linear_model import LogisticRegression
 #import pickle
 
 # load data
-data = read_csv('//Users//praveenkumarb//Desktop//p_train1.csv', header=None, names=['INSTRUCTION_OBTAINED_SCORE.STAAR_v2014_Cat_4_(A.7)(A)[R]',
+data = read_csv('p_train1.csv', header=None, names=['INSTRUCTION_OBTAINED_SCORE.STAAR_v2014_Cat_4_(A.7)(A)[R]',
                                                    'INSTRUCTION_OBTAINED_SCORE.STAAR_v2014_Cat_1_(A.11)(B)[R]','INSTRUCTION_OBTAINED_SCORE.STAAR_v2014_Cat_2_(A.3)(D)[R]',
                                                    'INSTRUCTION_OBTAINED_SCORE.STAAR_v2014_Cat_1_(A.10)(E)[R]','INSTRUCTION_OBTAINED_SCORE.STAAR_v2014_Cat_4_(A.6)(A)[R]',
                                                    'INSTRUCTION_OBTAINED_SCORE.STAAR_v2014_Cat_3_(A.2)(C)[R]','INSTRUCTION_OBTAINED_SCORE.STAAR_v2014_Cat_4_(A.7)(C)[R]',
