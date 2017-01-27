7e5662c53041fe6cede2e4a954b139c3bed11870
Praveen Kumar B <pbkumar2@in.ibm.com>
2017-01-27 08:33:50 -0600

    run-9

--- server.py
+++ server.py
@@ -36,6 +36,19 @@ from sklearn.feature_selection import RFE
 #from pandas import read_csv
 from sklearn.linear_model import LogisticRegression
 
+# load data
+data = pd.read_csv('p_train1.csv', header=None, names=['INSTRUCTION_OBTAINED_SCORE.STAAR_v2014_Cat_4_(A.7)(A)[R]',
+                                                   'INSTRUCTION_OBTAINED_SCORE.STAAR_v2014_Cat_1_(A.11)(B)[R]','INSTRUCTION_OBTAINED_SCORE.STAAR_v2014_Cat_2_(A.3)(D)[R]',
+                                                   'INSTRUCTION_OBTAINED_SCORE.STAAR_v2014_Cat_1_(A.10)(E)[R]','INSTRUCTION_OBTAINED_SCORE.STAAR_v2014_Cat_4_(A.6)(A)[R]',
+                                                   'INSTRUCTION_OBTAINED_SCORE.STAAR_v2014_Cat_3_(A.2)(C)[R]','INSTRUCTION_OBTAINED_SCORE.STAAR_v2014_Cat_4_(A.7)(C)[R]',
+                                                   'INSTRUCTION_OBTAINED_SCORE.STAAR_v2014_Cat_2_(A.3)(C)[R]','INSTRUCTION_OBTAINED_SCORE.STAAR_v2014_Cat_3_(A.5)(A)[R]',
+                                                   'INSTRUCTION_OBTAINED_SCORE.STAAR_v2014_Cat_3_(A.2)(I)[R]','INSTRUCTION_OBTAINED_SCORE.STAAR_v2014_Cat_3_(A.5)(C)[R]',
+                                                   'INSTRUCTION_OBTAINED_SCORE.STAAR_v2014_Cat_2_(A.3)(B)[R]','INSTRUCTION_OBTAINED_SCORE.STAAR_v2014_Cat_4_(A.8)(A)[R]',
+                                                   'INSTRUCTION_OBTAINED_SCORE.STAAR_v2014_Cat_5_(A.9)(C)[R]','INSTRUCTION_OBTAINED_SCORE.STAAR_v2014_Cat_3_(A.2)(A)[R]',
+                                                   'INSTRUCTION_OBTAINED_SCORE.STAAR_v2014_Cat_5_(A.9)(D)[R]','Risk'])
+
+print data
+
 ## code ends
 
 httpd = Server(("", PORT), Handler)
