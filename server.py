d92eb5519bf71bfeefb27b5862d394b6c39ec0fb
Praveen Kumar B <pbkumar2@in.ibm.com>
2017-01-28 00:34:36 -0600

    fff

--- server.py
+++ server.py
@@ -31,9 +31,7 @@ import pandas as pd
 import numpy as np
 import cPickle as pickle
 
-class C1:
-    def def1(self):
-        data = pd.read_csv('p_test1.csv', header=None, names=['Risk','INSTRUCTION_OBTAINED_SCORE.STAAR_v2014_Cat_4_(A.7)(A)[R]',
+data = pd.read_csv('p_test1.csv', header=None, names=['Risk','INSTRUCTION_OBTAINED_SCORE.STAAR_v2014_Cat_4_(A.7)(A)[R]',
                                                    'INSTRUCTION_OBTAINED_SCORE.STAAR_v2014_Cat_1_(A.11)(B)[R]','INSTRUCTION_OBTAINED_SCORE.STAAR_v2014_Cat_2_(A.3)(D)[R]',
                                                    'INSTRUCTION_OBTAINED_SCORE.STAAR_v2014_Cat_1_(A.10)(E)[R]','INSTRUCTION_OBTAINED_SCORE.STAAR_v2014_Cat_4_(A.6)(A)[R]',
                                                    'INSTRUCTION_OBTAINED_SCORE.STAAR_v2014_Cat_3_(A.2)(C)[R]','INSTRUCTION_OBTAINED_SCORE.STAAR_v2014_Cat_4_(A.7)(C)[R]',
@@ -42,43 +40,46 @@ class C1:
                                                    'INSTRUCTION_OBTAINED_SCORE.STAAR_v2014_Cat_2_(A.3)(B)[R]','INSTRUCTION_OBTAINED_SCORE.STAAR_v2014_Cat_4_(A.8)(A)[R]',
                                                    'INSTRUCTION_OBTAINED_SCORE.STAAR_v2014_Cat_5_(A.9)(C)[R]','INSTRUCTION_OBTAINED_SCORE.STAAR_v2014_Cat_3_(A.2)(A)[R]',
                                                    'INSTRUCTION_OBTAINED_SCORE.STAAR_v2014_Cat_5_(A.9)(D)[R]'])
-        print data
-        data[np.isnan(data)] = 0
-        print('\n')
 
-        with open('test3_model.pkl', 'rb') as f:
-            classifier = pickle.load(f)
+data[np.isnan(data)] = 0
+print data
 
-        del data['Risk']
-        print data
+print('\n')
+with open('test3_model.pkl', 'rb') as f:
+    classifier = pickle.load(f)
+    del data['Risk']
+    #print data
 
-        predicted_set = classifier.predict(data)
-        prob_predicted = classifier.predict_proba(data)
+    predicted_set = classifier.predict(data)
+    prob_predicted = classifier.predict_proba(data)
 
-        data = pd.DataFrame(data, columns=["INSTRUCTION_OBTAINED_SCORE.STAAR_v2014_Cat_4_(A.7)(A)[R]",
-                                                   "INSTRUCTION_OBTAINED_SCORE.STAAR_v2014_Cat_1_(A.11)(B)[R]","INSTRUCTION_OBTAINED_SCORE.STAAR_v2014_Cat_2_(A.3)(D)[R]",
-                                                   "INSTRUCTION_OBTAINED_SCORE.STAAR_v2014_Cat_1_(A.10)(E)[R]","INSTRUCTION_OBTAINED_SCORE.STAAR_v2014_Cat_4_(A.6)(A)[R]",
-                                                   "INSTRUCTION_OBTAINED_SCORE.STAAR_v2014_Cat_3_(A.2)(C)[R]","INSTRUCTION_OBTAINED_SCORE.STAAR_v2014_Cat_4_(A.7)(C)[R]",
-                                                   "INSTRUCTION_OBTAINED_SCORE.STAAR_v2014_Cat_2_(A.3)(C)[R]","INSTRUCTION_OBTAINED_SCORE.STAAR_v2014_Cat_3_(A.5)(A)[R]",
-                                                   "INSTRUCTION_OBTAINED_SCORE.STAAR_v2014_Cat_3_(A.2)(I)[R]","INSTRUCTION_OBTAINED_SCORE.STAAR_v2014_Cat_3_(A.5)(C)[R]",
-                                                   "INSTRUCTION_OBTAINED_SCORE.STAAR_v2014_Cat_2_(A.3)(B)[R]","INSTRUCTION_OBTAINED_SCORE.STAAR_v2014_Cat_4_(A.8)(A)[R]",
-                                                   "INSTRUCTION_OBTAINED_SCORE.STAAR_v2014_Cat_5_(A.9)(C)[R]","INSTRUCTION_OBTAINED_SCORE.STAAR_v2014_Cat_3_(A.2)(A)[R]",
-                                                   "INSTRUCTION_OBTAINED_SCORE.STAAR_v2014_Cat_5_(A.9)(D)[R]"])
-        pred = pd.DataFrame(predicted_set, columns=["Expected_Risk"])
-        df_prob = pd.DataFrame(prob_predicted, columns=["Risk_Prob_A", "Risk_Prob_B"])
+    data = pd.DataFrame(data, columns=["INSTRUCTION_OBTAINED_SCORE.STAAR_v2014_Cat_4_(A.7)(A)[R]",
+                                       "INSTRUCTION_OBTAINED_SCORE.STAAR_v2014_Cat_1_(A.11)(B)[R]",
+                                       "INSTRUCTION_OBTAINED_SCORE.STAAR_v2014_Cat_2_(A.3)(D)[R]",
+                                       "INSTRUCTION_OBTAINED_SCORE.STAAR_v2014_Cat_1_(A.10)(E)[R]",
+                                       "INSTRUCTION_OBTAINED_SCORE.STAAR_v2014_Cat_4_(A.6)(A)[R]",
+                                       "INSTRUCTION_OBTAINED_SCORE.STAAR_v2014_Cat_3_(A.2)(C)[R]",
+                                       "INSTRUCTION_OBTAINED_SCORE.STAAR_v2014_Cat_4_(A.7)(C)[R]",
+                                       "INSTRUCTION_OBTAINED_SCORE.STAAR_v2014_Cat_2_(A.3)(C)[R]",
+                                       "INSTRUCTION_OBTAINED_SCORE.STAAR_v2014_Cat_3_(A.5)(A)[R]",
+                                       "INSTRUCTION_OBTAINED_SCORE.STAAR_v2014_Cat_3_(A.2)(I)[R]",
+                                       "INSTRUCTION_OBTAINED_SCORE.STAAR_v2014_Cat_3_(A.5)(C)[R]",
+                                       "INSTRUCTION_OBTAINED_SCORE.STAAR_v2014_Cat_2_(A.3)(B)[R]",
+                                       "INSTRUCTION_OBTAINED_SCORE.STAAR_v2014_Cat_4_(A.8)(A)[R]",
+                                       "INSTRUCTION_OBTAINED_SCORE.STAAR_v2014_Cat_5_(A.9)(C)[R]",
+                                       "INSTRUCTION_OBTAINED_SCORE.STAAR_v2014_Cat_3_(A.2)(A)[R]",
+                                       "INSTRUCTION_OBTAINED_SCORE.STAAR_v2014_Cat_5_(A.9)(D)[R]"])
+    pred = pd.DataFrame(predicted_set, columns=["Expected_Risk"])
+    df_prob = pd.DataFrame(prob_predicted, columns=["Risk_Prob_A", "Risk_Prob_B"])
 
-        frame1 = [data,pred]
-        df1 = pd.concat(frame1,axis=1, join_axes=[data.index])
-        frame2 = [df1,df_prob]
-        df2 = pd.concat(frame2,axis=1, join_axes=[data.index])
-
-
-        df2['At_Risk'] = df2['Risk_Prob_B'].map(lambda x: 'Low' if x < 0.5 else 'Medium' if x < 0.75 else 'High')
-        #del df2['Risk']
-        print(df2)
-        return df2
-
-
+    frame1 = [data, pred]
+    df1 = pd.concat(frame1, axis=1, join_axes=[data.index])
+    frame2 = [df1, df_prob]
+    df2 = pd.concat(frame2, axis=1, join_axes=[data.index])
+    #print df2
+    df2['At_Risk'] = df2['Risk_Prob_B'].map(lambda x: 'Low' if x < 0.5 else 'Medium' if x < 0.75 else 'High')
+    print(df2)
+    #return df2
 
 ## code ends
 
