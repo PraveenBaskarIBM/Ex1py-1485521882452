e3e7542c16270f3a65935a3b445bf6ff374ed6f2
Praveen Kumar B <pbkumar2@in.ibm.com>
2017-01-30 01:45:53 -0600

    class

--- server.py
+++ server.py
@@ -82,6 +82,46 @@ with open('tests_model.pkl', 'rb') as f:
     #return df2
 ## code ends
 
+### FLASK CODE ####
+
+from flask import Flask, render_template
+app = Flask(__name__)
+app._static_folder = '/Users/praveenkumarb/Downloads/Ex1py/static'
+@app.route("/")
+def main():
+    return render_template("index.html", tables = [df2.to_html(classes ='table',index = False)])
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8019)
+   
+########### HTML CODE ############
+<!DOCTYPE html>
+<html lang="en" xmlns="http://www.w3.org/1999/html">
+
+<head>
+<title>Regression Output</title>
+</head>
+
+<body >
+<center><h2> RISK Output </h2></center>
+<center><div>
+<table border="0" style=" text-align: center" cellpadding="1" cellspacing="0" >
+{% for table in tables %}
+
+{{ table|safe }}
+{% endfor %}
+</table>
+</div></center>
+
+<center><footer class="footer">
+<!--<p>&copy; IBM India 2017</p>-->
+</footer></center>
+
+</div>
+</body>
+
+</html>  
+###########################################
+
 ##VCAP services End
 
 httpd = Server(("", PORT), Handler)
