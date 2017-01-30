619e54a68f5238be0fc18bd4bd2c9eb8114b495c
Praveen Kumar B <pbkumar2@in.ibm.com>
2017-01-30 01:10:39 -0600

    new files

--- server.py
+++ server.py
@@ -31,7 +31,7 @@ import pandas as pd
 import numpy as np
 import cPickle as pickle
 
-data = pd.read_csv('p_test1.csv', header=None, names=['Risk','INSTRUCTION_OBTAINED_SCORE.STAAR_v2014_Cat_4_(A.7)(A)[R]',
+data = pd.read_csv('s_test.csv', header=None, names=['Risk','INSTRUCTION_OBTAINED_SCORE.STAAR_v2014_Cat_4_(A.7)(A)[R]',
                                                    'INSTRUCTION_OBTAINED_SCORE.STAAR_v2014_Cat_1_(A.11)(B)[R]','INSTRUCTION_OBTAINED_SCORE.STAAR_v2014_Cat_2_(A.3)(D)[R]',
                                                    'INSTRUCTION_OBTAINED_SCORE.STAAR_v2014_Cat_1_(A.10)(E)[R]','INSTRUCTION_OBTAINED_SCORE.STAAR_v2014_Cat_4_(A.6)(A)[R]',
                                                    'INSTRUCTION_OBTAINED_SCORE.STAAR_v2014_Cat_3_(A.2)(C)[R]','INSTRUCTION_OBTAINED_SCORE.STAAR_v2014_Cat_4_(A.7)(C)[R]',
@@ -45,7 +45,7 @@ data[np.isnan(data)] = 0
 #print data
 
 print('\n')
-with open('test3_model.pkl', 'rb') as f:
+with open('tests_model.pkl', 'rb') as f:
     classifier = pickle.load(f)
     del data['Risk']
     #print data
@@ -81,16 +81,11 @@ with open('test3_model.pkl', 'rb') as f:
     print(df2)
     #return df2
 ## code ends
-
+##Flask code
 from flask import Flask, render_template
 app = Flask(__name__)
 app._static_folder = '/Users/praveenkumarb/Downloads/Ex1py/static'
 @app.route("/")
-def main():
-	return render_template('index.html', tables=[df.to_html(classes='table',index=False)])
-
-if __name__ == "df2":
-	app.run(host='0.0.0.0', port=8019)
 
 ##VCAP services End
 
