a0e28448c6a51756f2e7006f96e30af91a697d9a
Praveen Kumar B <pbkumar2@in.ibm.com>
2017-01-30 05:42:30 -0600

    changes

--- server.py
+++ server.py
@@ -81,17 +81,7 @@ with open('tests_model.pkl', 'rb') as f:
     print(df2)
     #return df2
 ## code ends
-###########################################
-### FLASK CODE ####
-
-from flask import Flask, render_template
-app = Flask(__name__)
-@app.route("/")
-def main():
-    return render_template("index.html", tables = [df.to_html(classes ='table',index = False)])
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8019) 
-    
+########################################### 
 ############################################    
 ##VCAP services End
 
